1e7f070fc98d57a75bc1e43cbed43a2fbf7b5a93
ZOI_dayo <zoizoi.dayo@gmail.com>
2022-03-27 15:43:19 +0900

    ざっと実装

---
+++ DataExtractor.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using Csv;
+using System.Linq;
+
+namespace DataExtractor
+{
+  public static class DataExtractor
+  {
+    private const string OutFile = @"path¥to¥File.csv";
+    private const string GaiaPath = @"path¥to¥Gaia¥";
+    private const string HipPath = @"path¥to¥Hip.csv";
+    private const string CrossMatchPath = @"path¥to¥CrossMatch.csv";
+
+    public static void Main()
+    {
+      using var outFStream = File.Open(OutFile, FileMode.OpenOrCreate);
+      // Gaia ID : Hipparcos ID
+      var crossMatch = new Dictionary<int, long>();
+      var crossMatchText = File.ReadAllText(CrossMatchPath);
+      foreach (ICsvLine line in CsvReader.ReadFromText(crossMatchText))
+      {
+        crossMatch[int.Parse(line[1])] = long.Parse(line[0]);
+      }
+
+      var gaiaFiles = GetGaiaFiles();
+      foreach (var gaiaFile in gaiaFiles)
+      {
+        using var fStream = File.OpenRead(GaiaPath + gaiaFile);
+        using var gStream = new GZipStream(fStream, CompressionMode.Decompress);
+        using var reader = new StreamReader(gStream);
+        var isFirstLine = true;
+        while (reader.EndOfStream == false)
+        {
+          // 1行目はタイトルなのでスキップする
+          if (isFirstLine)
+          {
+            isFirstLine = false;
+            continue;
+          }
+
+          var line = reader.ReadLine();
+          if (line == null) continue;
+          var strData = line.Split(',');
+          outFStream.Write(
+            StarData.FromGaiaProperty(
+              double.Parse(strData[5]),
+              double.Parse(strData[7]),
+              float.Parse(strData[69]),
+              float.Parse(strData[74]),
+              float.Parse(strData[79])
+            ).ToByte(), 0, 24);
+        }
+      }
+
+      // https://cdsarc.cds.unistra.fr/viz-bin/cat/I/311#/browse
+      using (var fStream = File.OpenRead(HipPath))
+      using (var gStream = new GZipStream(fStream, CompressionMode.Decompress))
+      using (var reader = new StreamReader(gStream))
+      {
+        while (reader.EndOfStream == false)
+        {
+          var line = reader.ReadLine();
+          if (line == null) continue;
+          var id = int.Parse(line[..7]);
+          if (crossMatch.ContainsKey(id)) continue;
+          // B-V?V-B?
+          // キャスト
+          outFStream.Write(
+            new StarData(
+              double.Parse(line[15..29]),
+              double.Parse(line[29..43]),
+              float.Parse(line[129..137]),
+              -1 * float.Parse(line[152..159])
+            ).ToByte(), 0, 24);
+        }
+      }
+    }
+
+    private static IEnumerable<string> GetGaiaFiles()
+    {
+      // 今回は、一緒にダウンロードした_MD5SUM.txtからファイル名を抜き出して使います
+      var sr = new StreamReader(GaiaPath + "_MD5SUM.txt");
+      var md5SumContent = sr.ReadToEnd();
+      sr.Close();
+      return md5SumContent.Split('\n').Select(s => s.Split(' ')[1]);
+    }
+  }
+}
---
+++ StarData.cs
@@ -0,0 +1,47 @@
+using System;
+
+namespace DataExtractor
+{
+  public class StarData
+  {
+    private readonly double _ra; // 赤経
+    private readonly double _dec; // 赤緯
+    private readonly float _vMag; // 等級
+    private readonly float _bvColor; // 色指数
+
+    public StarData(double ra, double dec, float vMag, float bvColor)
+    {
+      _ra = ra;
+      _dec = dec;
+      _vMag = vMag;
+      _bvColor = bvColor;
+    }
+
+    public static StarData FromGaiaProperty(double ra, double dec, float phot_g_mean_mag, float phot_bp_mean_mag,
+      float phot_rp_mean_mag)
+    {
+      var G_HP = 0.0149 * Math.Pow(phot_bp_mean_mag - phot_rp_mean_mag, 3)
+                 - 0.12 * Math.Pow(phot_bp_mean_mag - phot_rp_mean_mag, 2)
+                 - 0.2344 * (phot_bp_mean_mag - phot_rp_mean_mag)
+                 - 0.01968;
+      var bvColor = (float)(-0.5 <= G_HP
+        ? -2.4 * G_HP - 0.075
+        : -2.1 * Math.Pow(G_HP, 2) - 4.2 * G_HP - 0.45);
+      return new StarData(ra, dec, phot_g_mean_mag, bvColor);
+    }
+
+    //こいつはいらない?
+    // J2000.0変換のみできたらいいけどそれはGaiaも同じだから
+    // 別関数として独立させてしまえばいい?
+
+    public byte[] ToByte()
+    {
+      var data = new byte[24];
+      Buffer.BlockCopy(BitConverter.GetBytes(_ra), 0, data, 0, 8);
+      Buffer.BlockCopy(BitConverter.GetBytes(_dec), 0, data, 8, 8);
+      Buffer.BlockCopy(BitConverter.GetBytes(_vMag), 0, data, 16, 4);
+      Buffer.BlockCopy(BitConverter.GetBytes(_bvColor), 0, data, 20, 4);
+      return data;
+    }
+  }
+}
